654ddb04cc3e14c6c8e2a8833918791a00fd2de6
Chris Qualls <39442409+Bestlogo56@users.noreply.github.com>
2019-01-07 10:17:01 -0600

    Update RobotMap.java

--- WorkshopBot/src/main/java/frc/robot/RobotMap.java
+++ WorkshopBot/src/main/java/frc/robot/RobotMap.java
@@ -17,7 +17,7 @@ public class RobotMap {
   // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+  public static int wilisgay = 2;
   //public static int 
   // If you are using multiple modules, make sure to define both the port
   // number and the module. For example you with a rangefinder:
